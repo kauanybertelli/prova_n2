9b3f14410689949ba327a035cf8e70395ee02d12
kauanybertelli <kau.bertelli@hotmail.com>
2025-06-08 19:36:18 -0300

    refactor - adicionado .gitignore e atualizado arquivo .index tirando comentários.

--- backend/index.js
+++ backend/index.js
@@ -20,13 +20,3 @@ app.get('/usuarios/:id', (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`API rodando na porta ${PORT}`));
-
-
-// PARA RODAR O PROJETO ACESSAR A PASTA ONDE O PROJETO ESTÁ NESSE CASO: "C:\Unidep\2025\prova_n2\backend"
-// E RODAR O COMANDO: npm install --force (irá criar a pasta node_modules)
-// depois o comando npm start
-// OBS: PODE SER PELO TERMINAL
-
-// para o frontend é a mesma coisa, porém acessar a pasta frontend.
-// e o caminho está em: C:\Unidep\2025\revisao_n2\frontend.
-// e acessar no navegador o endereço: http://localhost:3000/
